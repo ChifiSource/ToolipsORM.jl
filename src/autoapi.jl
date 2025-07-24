842c4124ef057d8519789b92bf9477d0ecba34b9
emmy <52672675+emmaccode@users.noreply.github.com>
2025-07-24 13:01:35 -0600

    updated environment

--- src/autoapi.jl
+++ src/autoapi.jl
@@ -1,6 +1,6 @@
-ORM_API = Toolips.QuickExtension{:ORMAPI}()
-
-struct ORMAPI <: Toolips.AbstractExtension end
+struct ORMAPI{T <: AbstractCursorDriver} <: Toolips.AbstractExtension 
+    orm::ORM{T}
+end
 
 
 function on_start(ext::Toolips.QuickExtension{:ORMAPI}, data::Dict{Symbol, Any}, routes::Vector{<:AbstractRoute})
